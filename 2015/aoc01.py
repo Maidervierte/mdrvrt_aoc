ecfdd4668564f8f71befddedf6a548c7e402ec34
Erik L <lindstae@rhrk.uni-kl.de>
2023-08-30 01:47:30 +0200

    2015 aoc01

---
+++ 2015/aoc01.py
@@ -0,0 +1,19 @@
+"""
+aoc01
+"""
+
+with open("input01.txt", "r", encoding="utf-8") as f:
+    number_list = f.read()
+
+floor = 0
+answer2 = 0
+for index, x in enumerate(number_list):
+    if x == "(":
+        floor += 1
+    elif x == ")":
+        floor -= 1
+    if floor == -1 and answer2 == 0:
+        answer2 = index + 1
+
+print("Answer 1:", floor)
+print("Answer 2:", answer2)
