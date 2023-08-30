f5173c6cd478f7f6ae139977d9d5936ac9920d4b
Erik L <lindstae@rhrk.uni-kl.de>
2023-08-30 18:18:50 +0200

    2015 aoc13 removed unnecessary comment

--- 2015/aoc13.py
+++ 2015/aoc13.py
@@ -4,19 +4,6 @@ from itertools import permutations
 with open("input13.txt", "r", encoding="utf-8") as f:
     happiness = f.read().splitlines()
 
-# happiness = "Alice would gain 54 happiness units by sitting next to Bob.\n" \
-#             "Alice would lose 79 happiness units by sitting next to Carol.\n" \
-#             "Alice would lose 2 happiness units by sitting next to David.\n" \
-#             "Bob would gain 83 happiness units by sitting next to Alice.\n" \
-#             "Bob would lose 7 happiness units by sitting next to Carol.\n" \
-#             "Bob would lose 63 happiness units by sitting next to David.\n" \
-#             "Carol would lose 62 happiness units by sitting next to Alice.\n" \
-#             "Carol would gain 60 happiness units by sitting next to Bob.\n" \
-#             "Carol would gain 55 happiness units by sitting next to David.\n" \
-#             "David would gain 46 happiness units by sitting next to Alice.\n" \
-#             "David would lose 7 happiness units by sitting next to Bob.\n" \
-#             "David would gain 41 happiness units by sitting next to Carol.\n".splitlines()
-
 hap_dict = {}
 name_set = set()
 for line in happiness:
