ec2115b7a2b121b9f052b8796d940ae05cab7788
Maidervierte <68083029+Maidervierte@users.noreply.github.com>
2023-12-05 07:01:10 +0100

    2023 aoc05

--- 2023/aoc05.py
+++ 2023/aoc05.py
@@ -25,44 +25,16 @@ with open("input05htl", "r", encoding="utf-8") as f:
     loc_input = f.read().splitlines()
 
 seeds = [int(x) for x in input_list[0].split()[1:]]
+inputs = [soil_input, fert_input, water_input, light_input, temp_input, hum_input, loc_input]
 min_loc = 9999999999999999999
-for seed in seeds:
-    for line in soil_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= seed <= source_range_start + range_length:
-            soil = dest_range_start + (seed - source_range_start)
-            break
-    for line in fert_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= soil <= source_range_start + range_length:
-            fert = dest_range_start + (soil - source_range_start)
-            break
-    for line in water_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= fert <= source_range_start + range_length:
-            water = dest_range_start + (fert - source_range_start)
-            break
-    for line in light_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= water <= source_range_start + range_length:
-            light = dest_range_start + (water - source_range_start)
-            break
-    for line in temp_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= light <= source_range_start + range_length:
-            temp = dest_range_start + (light - source_range_start)
-            break
-    for line in hum_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= temp <= source_range_start + range_length:
-            hum = dest_range_start + (temp - source_range_start)
-            break
-    for line in loc_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= hum <= source_range_start + range_length:
-            loc = dest_range_start + (hum - source_range_start)
-            break
-    min_loc = min(min_loc, loc)
+for item in seeds:
+    for input_map in inputs:
+        for line in input_map:
+            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
+            if source_range_start <= item <= source_range_start + range_length:
+                item = dest_range_start + (item - source_range_start)
+                break
+    min_loc = min(min_loc, item)
 
 print("Answer 1:", min_loc)
 
@@ -73,85 +45,25 @@ min_seed = 0
 while i < len(seed_ranges):
     for x in range(0, seed_ranges[i + 1], 50000):
         seed = seed_ranges[i] + x
-        for line in soil_input:
-            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-            if source_range_start <= seed <= source_range_start + range_length:
-                soil = dest_range_start + (seed - source_range_start)
-                break
-        for line in fert_input:
-            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-            if source_range_start <= soil <= source_range_start + range_length:
-                fert = dest_range_start + (soil - source_range_start)
-                break
-        for line in water_input:
-            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-            if source_range_start <= fert <= source_range_start + range_length:
-                water = dest_range_start + (fert - source_range_start)
-                break
-        for line in light_input:
-            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-            if source_range_start <= water <= source_range_start + range_length:
-                light = dest_range_start + (water - source_range_start)
-                break
-        for line in temp_input:
-            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-            if source_range_start <= light <= source_range_start + range_length:
-                temp = dest_range_start + (light - source_range_start)
-                break
-        for line in hum_input:
-            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-            if source_range_start <= temp <= source_range_start + range_length:
-                hum = dest_range_start + (temp - source_range_start)
-                break
-        for line in loc_input:
-            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-            if source_range_start <= hum <= source_range_start + range_length:
-                loc = dest_range_start + (hum - source_range_start)
-                break
-        if loc < min_loc:
-            min_loc = loc
+        item = seed
+        for input_map in inputs:
+            for line in input_map:
+                dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
+                if source_range_start <= item <= source_range_start + range_length:
+                    item = dest_range_start + (item - source_range_start)
+                    break
+        if item < min_loc:
+            min_loc = item
             min_seed = seed
     i += 2
 
-for x in range(min_seed - 60000, min_seed + 60000):
-    seed = x
-    for line in soil_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= seed <= source_range_start + range_length:
-            soil = dest_range_start + (seed - source_range_start)
-            break
-    for line in fert_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= soil <= source_range_start + range_length:
-            fert = dest_range_start + (soil - source_range_start)
-            break
-    for line in water_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= fert <= source_range_start + range_length:
-            water = dest_range_start + (fert - source_range_start)
-            break
-    for line in light_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= water <= source_range_start + range_length:
-            light = dest_range_start + (water - source_range_start)
-            break
-    for line in temp_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= light <= source_range_start + range_length:
-            temp = dest_range_start + (light - source_range_start)
-            break
-    for line in hum_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= temp <= source_range_start + range_length:
-            hum = dest_range_start + (temp - source_range_start)
-            break
-    for line in loc_input:
-        dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
-        if source_range_start <= hum <= source_range_start + range_length:
-            loc = dest_range_start + (hum - source_range_start)
-            break
-    if loc < min_loc:
-        min_loc = loc
-        min_seed = seed
+for item in range(min_seed - 50000, min_seed + 50000):
+    for input_map in inputs:
+        for line in input_map:
+            dest_range_start, source_range_start, range_length = [int(x) for x in line.split()]
+            if source_range_start <= item <= source_range_start + range_length:
+                item = dest_range_start + (item - source_range_start)
+                break
+    min_loc = min(min_loc, item)
 
 print("Answer 2:", min_loc)
