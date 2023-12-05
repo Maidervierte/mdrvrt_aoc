7a521e9846cfe6fdbc5a1c4973fff9b5f2cae675
Maidervierte <68083029+Maidervierte@users.noreply.github.com>
2023-12-05 06:50:47 +0100

    2023 aoc04

--- 2023/aoc04.py
+++ 2023/aoc04.py
@@ -7,7 +7,7 @@ matches = {}
 answer1 = 0
 won_cards = []
 for i, line in enumerate(input_list):
-    won_cards.append(i + 1)
+    won_cards.append(1)
     intersection = set(int(x) for x in line.split(" | ")[0].split(": ")[1].split()) \
         .intersection(int(x) for x in line.split(" | ")[1].split())
     matches[i + 1] = len(intersection)
@@ -17,12 +17,8 @@ for i, line in enumerate(input_list):
 
 print("Answer 1:", answer1)
 
-i = 0
-while i < len(won_cards):
-    cur_card = won_cards[i]
-    intersection = matches[cur_card]
-    for x in range(intersection):
-        won_cards.append(cur_card + x + 1)
-    i += 1
+for i in range(len(won_cards)):
+    for x in range(matches[i + 1]):
+        won_cards[i + x + 1] += won_cards[i]
 
-print("Answer 2:", len(won_cards))
+print("Answer 2:", sum(won_cards))
