66f6b9bd095ca4c150c0d7ee14e4043f065c4968
Maidervierte <68083029+Maidervierte@users.noreply.github.com>
2025-12-03 21:42:40 +0100

    aoc2025 fixed input reading

--- 2025/aoc01.py
+++ 2025/aoc01.py
@@ -1,22 +1,22 @@
 """ 2025 aoc01 """
 
 with open("input01.txt", "r", encoding="utf-8") as f:
-    input_list = [x.split("   ") for x in f.read().splitlines()]
+    input_list = f.read().splitlines()
 
 answer1 = 0
 answer2 = 0
 
 dial = 50
 for line in input_list:
-    if line[0][0] == "L":
-        for i in range( int(line[0][1:])):
+    if line[0] == "L":
+        for i in range( int(line[1:])):
             dial-=1
             if dial<0:
                 dial=99
             if dial == 0:
                 answer2+=1
-    if line[0][0] == "R":
-        for i in range( int(line[0][1:])):
+    if line[0] == "R":
+        for i in range( int(line[1:])):
             dial+=1
             if dial>99:
                 dial=0
