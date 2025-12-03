7af8e361f34c2cdf704c78f868209e1c45b76bcc
Maidervierte <68083029+Maidervierte@users.noreply.github.com>
2025-12-03 21:39:57 +0100

    aoc2025 01

--- 2025/aoc01.py
+++ 2025/aoc01.py
@@ -3,21 +3,30 @@
 with open("input01.txt", "r", encoding="utf-8") as f:
     input_list = [x.split("   ") for x in f.read().splitlines()]
 
-list1 = []
-list2 = []
-
-for line in input_list:
-    list1.append(int(line[0]))
-    list2.append(int(line[1]))
-
 answer1 = 0
 answer2 = 0
-list1.sort()
-list2.sort()
-for i in range(len(input_list)):
-    answer1 += abs(list1[i] - list2[i])
-    temp = list2.count(list1[i])
-    answer2 += (list1[i] * temp)
+
+dial = 50
+for line in input_list:
+    if line[0][0] == "L":
+        for i in range( int(line[0][1:])):
+            dial-=1
+            if dial<0:
+                dial=99
+            if dial == 0:
+                answer2+=1
+    if line[0][0] == "R":
+        for i in range( int(line[0][1:])):
+            dial+=1
+            if dial>99:
+                dial=0
+            if dial == 0:
+                answer2+=1
+    if dial == 0:
+        answer1 += 1
+
+if dial == 0:
+    answer2 += 1
 
 print("Answer 1:", answer1)
 print("Answer 2:", answer2)
